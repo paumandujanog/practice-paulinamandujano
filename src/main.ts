84b1816c9725b36bea307c92434c3e49e96c92fc
Paulina Mandujano <paumandujano.g@gmail.com>
2023-05-11 18:00:48 -0600

    add new functions

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import App from './App.vue';
 import router from './router';
 import { connect } from '../database/database';
 
-createApp(App).use(router).mount('#app')
-
 async function start() {
-    await connect();
-
     const app = createApp(App);
     app.use(router);
     app.mount('#app');
